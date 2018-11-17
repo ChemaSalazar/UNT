383feb4be517f7ce92a9d6b762c356da6a8fe54f
ChemaSalazar <chemasalazar@outlook.com>
2018-11-17 03:51:16 -0600

    :wrench: Added DHCP structure & Definitions

--- 3530/Program3/UDPClient.c
+++ 3530/Program3/UDPClient.c
@@ -2,21 +2,39 @@
    Execution  : ./UDPclient <port_number> [eg. port_number = 5000, where port_number is the UDP server port number]
 */
 
+//Import
+/*============================================================================*/
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
- 
+
+//Definitions
+/*============================================================================*/
 #define SERVER "129.120.151.94" //IP address of server
 #define BUFLEN 512  //Max length of buffer
- 
+
+//Classes && Structs
+/*============================================================================*/
+struct DHCP_Request {
+  char* yiaddr;
+  int tid;  //Transaction ID
+  int ttl;  //Time to live
+};
+
+//Function Declarations && Implementations
+/*============================================================================*/
 void die(char *s)
 {
     perror(s);
     exit(1);
 }
- 
+
+
+
+//Main Function
+/*============================================================================*/
 int main(int argc, char *argv[])
 {
     struct sockaddr_in si_other;
@@ -26,23 +44,23 @@ int main(int argc, char *argv[])
 
     system("clear");
     printf("...This is UDP client...\n\n");
- 
+
     if ( (s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
     {
         die("socket");
     }
- 
+
     memset((char *) &si_other, 0, sizeof(si_other));
     portno = atoi(argv[1]); //The port on which to listen for incoming data
     si_other.sin_family = AF_INET;
     si_other.sin_port = htons(portno);
-     
-    if (inet_aton(SERVER , &si_other.sin_addr) == 0) 
+
+    if (inet_aton(SERVER , &si_other.sin_addr) == 0)
     {
         fprintf(stderr, "inet_aton() failed\n");
         exit(1);
     }
- 
+
     // Sending the message to the server
     printf("Enter client's message : ");
     gets(message);
@@ -50,7 +68,7 @@ int main(int argc, char *argv[])
     {
         die("sendto()");
     }
-         
+
     //Receiving reply from server and printing it
     //clear the buffer by filling null, it might have previously received data
     bzero(buf, BUFLEN);
@@ -59,7 +77,7 @@ int main(int argc, char *argv[])
        die("recvfrom()");
     }
     printf("Server has sent: %s\n", buf);
-     
+
     close(s);
     return 0;
 }
--- 3530/Program3/UDPServer.c
+++ 3530/Program3/UDPServer.c
@@ -2,47 +2,113 @@
    Execution  : ./UDPserver <port_number> [eg. port_number = 5000, where port_number is the UDP server port number]
 */
 
-#include <stdio.h> 
+//Import
+/*============================================================================*/
+#include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
- 
+
+//Definitions
+/*============================================================================*/
 #define BUFLEN 512  //Max length of buffer
- 
+#define MAXCLIENT 10 //Max number of supported clients
+#define TIMETOLIVE 3600 //Time to live
+
+//Classes && Structs
+/*============================================================================*/
+
+struct DHCP_Responce {
+  char* yiaddr;
+  int tid; //Transaction ID
+  int ttl; //Time to live
+};
+
+//Function Declarations && Implementations
+/*============================================================================*/
+
 void die(char *s)
 {
     perror(s);
     exit(1);
 }
- 
+
+void m(int x){
+
+  //more messages to be added as program develops.
+  switch(x)
+  {
+    case 0 :
+      printf("[STATUS]: Success\n");
+      break;
+    case 1 :
+      printf("[STATUS]: Fail\n");
+      break;
+    case 2 :
+      printf("[TASK]: DHCP Discovered\n");
+      break;
+    case 3 :
+      printf("[TASK]: DHCP Offer Sent\n");
+      break;
+    case 4 :
+      printf("[TASK]: DHCP Request Received\n");
+      break;
+    case 5 :
+      printf("[TASK]: DHCP Acknowledgement Sent\n");
+      break;
+  }
+}
+
+
+void set_IP(int client)
+{
+  if(client >= MAXCLIENT)
+  {
+    m(1);
+  }
+  else
+  {
+    //TODO:
+    //open file for IP address
+    //check for available address
+    //set address to current clients
+    //update list of available addresses
+    //close file
+  }
+  return;
+}
+
+
+//Main Function
+/*============================================================================*/
 int main(int argc, char *argv[])
 {
     struct sockaddr_in si_me, si_other;
-     
+
     int s, i, slen = sizeof(si_other) , recv_len, portno;
     char buf[BUFLEN], message[1024];
-     
+
     //create a UDP socket
     if ((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
     {
         die("socket");
     }
-     
+
     // zero out the structure
     memset((char *) &si_me, 0, sizeof(si_me));
     portno = atoi(argv[1]); //The port on which to listen for incoming data
-     
+
     si_me.sin_family = AF_INET;
     si_me.sin_port = htons(portno);
     si_me.sin_addr.s_addr = htonl(INADDR_ANY);
-     
+
     //bind socket to port
     if( bind(s , (struct sockaddr*)&si_me, sizeof(si_me) ) == -1)
     {
         die("bind");
     }
-    
+
     system("clear");
     printf("...This is UDP server...\n\n");
 
@@ -51,17 +117,18 @@ int main(int argc, char *argv[])
     {
         printf("Waiting for client's message...\n\n");
         fflush(stdout);
-         
+
         //Receiving data from client
         if ((recv_len = recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen)) == -1)
         {
             die("recvfrom()");
+
         }
-         
+        m(2);
         //print details of the client/peer and the data received
         printf("Received packet from %s, port number:%d\n", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
         printf("Client has sent: %s\n", buf);
-         
+
         //Sending reply to the client
 	bzero(message, 1024);
 	printf("Enter server's message:");
@@ -69,9 +136,11 @@ int main(int argc, char *argv[])
         if (sendto(s, message, strlen(message), 0, (struct sockaddr*) &si_other, slen) == -1)
         {
             die("sendto()");
+
         }
+            m(3);
     }
- 
     close(s);
+
     return 0;
 }
